96e9371badfc717450a4d52020fe1a78aa10d516
Barry <barry.teoh@perceptionz.net>
2014-06-26 17:15:55 +1000

    Latest version of node

--- install-mongo.sh
+++ install-mongo.sh
@@ -3,4 +3,5 @@ sudo su
 apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
 
+apt-get -y update
 apt-get install -y mongodb-10gen
--- nvm-provision.sh
+++ nvm-provision.sh
@@ -5,5 +5,5 @@ sudo apt-get install -y git-core curl
 sudo su vagrant -c "wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh"
 
 source /home/vagrant/.profile
-nvm install 0.10.4
-nvm alias default 0.10.4
+nvm install 0.10.29
+nvm alias default 0.10.29
